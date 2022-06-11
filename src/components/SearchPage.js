05db1ebb4724d07aa4aaa5dad26cfe48916028c5
ahnsunnyyoung <ahnsunnyyoung@gmail.com>
2022-06-12 01:20:18 +0900

    cors 관련 revert

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -83,8 +83,8 @@ function SearchPage() {
   const [myTheme, setTheme] = useState(prefersDarkMode ? darkTheme : lightTheme);
   const [page, setPage] = useState(1);
   
-  const GET_URL = process.env.REACT_APP_DB_HOST+'/search/blog.json'
-  const POST_URL = process.env.REACT_APP_DB_HOST+'/classification/blog.json'
+  const GET_URL = 'http://3.39.137.73:8080/search/blog.json'
+  const POST_URL = 'http://3.39.137.73:8080/classification/blog.json'
   const [reviews, setReviews] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
